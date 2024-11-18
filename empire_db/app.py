7972550db1a04ecad845258600e44b1a7fa4f067
mu7s <jm205352@umconnect.umt.edu>
2024-11-18 03:13:19 +0000

    added in bridge table with links to add new assignments

--- empire_db/app.py
+++ empire_db/app.py
@@ -1,165 +1,156 @@
 from flask import Flask, render_template, request, redirect, url_for
-
 import mysql.connector
-
 import os
-
 from dotenv import load_dotenv
-
 from markupsafe import escape
 
 # Load environment variables
-
 load_dotenv()
 
 app = Flask(__name__)
 
 # Database connection function
-
 def get_db_connection():
-
     return mysql.connector.connect(
-
         host=os.getenv("DB_HOST"),
-
         user=os.getenv("DB_USER"),
-
         password=os.getenv("DB_PASSWORD"),
-
         database=os.getenv("DB_NAME")
-
     )
 
-# Read: Display table data
-
+# Existing CRUD routes for Person table
 @app.route('/')
-
 def index():
-
     conn = get_db_connection()
-
     cursor = conn.cursor(dictionary=True)
-
     cursor.execute("SELECT * FROM Person")
-
     persons = cursor.fetchall()
-
     conn.close()
-
     return render_template('index.html', persons=persons)
 
-# Create: Handle form submission for new data
-
 @app.route('/add_person', methods=['POST'])
-
 def add_person():
-
     first_name = escape(request.form['first_name'])
-
     last_name = escape(request.form['last_name'])
-
     age = request.form['age']
-
     rank_id = request.form['rank_id']
-
     specialization_id = request.form['specialization_id']
-
     
-
     conn = get_db_connection()
-
     cursor = conn.cursor()
-
     cursor.execute(
-
         "INSERT INTO Person (FirstName, LastName, Age, RankID, SpecializationID) VALUES (%s, %s, %s, %s, %s)",
-
         (first_name, last_name, age, rank_id, specialization_id)
-
     )
-
     conn.commit()
-
     conn.close()
-
     return redirect(url_for('index'))
-
-# Update: Load form with pre-filled data for editing
 
 @app.route('/edit_person/<int:person_id>', methods=['GET', 'POST'])
-
 def edit_person(person_id):
-
     conn = get_db_connection()
-
     cursor = conn.cursor(dictionary=True)
-
-    
-
     if request.method == 'POST':
-
-        # Update data
-
         first_name = escape(request.form['first_name'])
-
         last_name = escape(request.form['last_name'])
-
         age = request.form['age']
-
         rank_id = request.form['rank_id']
-
         specialization_id = request.form['specialization_id']
-
         
-
         cursor.execute(
-
             "UPDATE Person SET FirstName = %s, LastName = %s, Age = %s, RankID = %s, SpecializationID = %s WHERE PersonID = %s",
-
             (first_name, last_name, age, rank_id, specialization_id, person_id)
-
         )
-
         conn.commit()
-
         conn.close()
-
         return redirect(url_for('index'))
-
     else:
-
-        # Load data for pre-filling the form
-
         cursor.execute("SELECT * FROM Person WHERE PersonID = %s", (person_id,))
-
         person = cursor.fetchone()
-
         conn.close()
-
         return render_template('edit_person.html', person=person)
 
-# Delete: Remove a specific row
-
 @app.route('/delete_person/<int:person_id>', methods=['POST'])
-
 def delete_person(person_id):
-
     conn = get_db_connection()
-
     cursor = conn.cursor()
-
     cursor.execute("DELETE FROM Person WHERE PersonID = %s", (person_id,))
-
     conn.commit()
-
     conn.close()
-
     return redirect(url_for('index'))
 
-# Start the Flask server
+# New routes for Assignment table (many-to-many relationship)
 
+# List all people
+@app.route('/people')
+def list_people():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM Person")
+    people = cursor.fetchall()
+    conn.close()
+    return render_template('people.html', people=people)
+
+# List all assignments for a specific person
+@app.route('/person_assignments/<int:person_id>')
+def person_assignments(person_id):
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM Person WHERE PersonID = %s", (person_id,))
+    person = cursor.fetchone()
+    cursor.execute("""
+        SELECT Location.PlanetName, Assignment.StartDate, Assignment.EndDate, Assignment.LocationID
+        FROM Assignment
+        JOIN Location ON Assignment.LocationID = Location.LocationID
+        WHERE Assignment.PersonID = %s
+    """, (person_id,))
+    assignments = cursor.fetchall()
+    conn.close()
+    return render_template('person_assignments.html', person=person, assignments=assignments)
+
+# List all people assigned to a specific location
+@app.route('/location_assignments/<int:location_id>')
+def location_assignments(location_id):
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM Location WHERE LocationID = %s", (location_id,))
+    location = cursor.fetchone()
+    cursor.execute("""
+        SELECT Person.FirstName, Person.LastName, Assignment.StartDate, Assignment.EndDate 
+        FROM Assignment
+        JOIN Person ON Assignment.PersonID = Person.PersonID
+        WHERE Assignment.LocationID = %s
+    """, (location_id,))
+    people = cursor.fetchall()
+    conn.close()
+    return render_template('location_assignments.html', location=location, people=people)
+
+# Add a new assignment
+@app.route('/add_assignment', methods=['GET', 'POST'])
+def add_assignment():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    if request.method == 'POST':
+        person_id = request.form['person_id']
+        location_id = request.form['location_id']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        cursor.execute("""
+            INSERT INTO Assignment (PersonID, LocationID, StartDate, EndDate)
+            VALUES (%s, %s, %s, %s)
+        """, (person_id, location_id, start_date, end_date))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('list_people'))
+    else:
+        cursor.execute("SELECT * FROM Person")
+        people = cursor.fetchall()
+        cursor.execute("SELECT * FROM Location")
+        locations = cursor.fetchall()
+        conn.close()
+        return render_template('add_assignment.html', people=people, locations=locations)
+
+# Run Flask application
 if __name__ == '__main__':
-
     app.run(debug=True)
-
 
